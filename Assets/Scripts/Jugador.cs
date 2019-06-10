bf4692426845cba96d700a048b9ed3e193f6ddef
Irene Cordova <irene_cordova@outlook.es>
2019-06-09 22:14:58 -0500

    [Finish]

---
+++ Assets/Scripts/Jugador.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jugador : MonoBehaviour
+{
+
+    public Rigidbody2D body;
+    public float walkingSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         
+        var v = body.velocity;
+        var speed = 0f;
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            speed += -walkingSpeed;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            speed += +walkingSpeed;
+        }
+
+        v.x = speed;
+        body.velocity = v;
+        
+    }
+}
---
+++ Assets/Scripts/Pelota.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Pelota : MonoBehaviour
+{
+    public Rigidbody2D body;
+    public float Vel = 5f;
+    public GameObject floor;
+    public GameObject gameover;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        body.velocity = Vector2.down * Vel;
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject == floor)
+        {
+            body.velocity = Vector2.zero * Vel;
+            gameover.SetActive(true);
+        }
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
